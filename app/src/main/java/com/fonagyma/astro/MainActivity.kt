c872b8d083a3f9b0e4144032d4cdd1f54014262b
fhadnagy <fonagymarci@gmail.com>
2022-11-07 20:24:24 +0100

    will start by making an example canvas view fragment game

---
+++ app/src/main/java/com/fonagyma/astro/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.fonagyma.astro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
