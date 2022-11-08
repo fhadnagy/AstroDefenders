20e6c6721016924a775eb07355de68b671de7884
fhadnagy <fonagymarci@gmail.com>
2022-11-08 20:00:34 +0100

    wtf is this

---
+++ app/src/main/java/com/fonagyma/astro/GameObject.kt
@@ -0,0 +1,27 @@
+package com.fonagyma.astro
+
+import android.graphics.PointF
+
+class GameObject (direction: PointF){
+
+        private var velocity: PointF = PointF()
+        val position: PointF = PointF()
+
+        init {
+            // Determine the direction
+            velocity.x = direction.x
+            velocity.y = direction.y
+        }
+
+        fun force(a: PointF){
+            velocity.x+=a.x
+            velocity.y+=a.y
+        }
+
+        fun update() {
+            // Move the particle
+            position.x += velocity.x
+            position.y += velocity.y
+        }
+
+}
---
+++ app/src/main/java/com/fonagyma/astro/ObjectSystem.kt
@@ -0,0 +1,96 @@
+package com.fonagyma.astro
+
+
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.PointF
+import java.lang.Math.sqrt
+import java.util.*
+import kotlin.math.sqrt
+
+
+class ObjectSystem(){
+
+    var gameObjects:
+            ArrayList<GameObject> = ArrayList()
+    private val random = Random()
+    var sp = PointF()
+    var drawing = false
+    var afterD = false
+    var clear= false
+    private val gC = 10f
+    lateinit var midPointF: PointF
+
+   constructor(mp: PointF) : this() {
+        midPointF=mp
+    }
+
+    fun addObject(startPosition: PointF){
+            sp= startPosition
+            afterD= true
+    }
+
+    fun update() {
+        for (g in gameObjects) {
+            var a= PointF(midPointF.x-g.position.x,midPointF.y-g.position.y)
+            val xr = 1/sqrt(a.x*a.x+a.y*a.y)
+            a.x = gC*xr*xr*xr
+            g.force(a)
+            g.update()
+        }
+    }
+
+    fun draw(canvas: Canvas, paint: Paint) {
+        drawing= true
+        for (g in gameObjects) {
+            // Option 1 - Colored particles
+            paint.setARGB(255, random.nextInt(256),
+                random.nextInt(256),
+                random.nextInt(256))
+            // Option 2 - White particles
+            //paint.color = Color.argb(255, 255, 255, 255)
+            // How big is each particle?
+            // Option 1 - Big particles
+            //val sizeX = 25f
+            //val sizeY = 25f
+            // Option 2 - Medium particles
+            //val sizeX = 10f
+            //val sizeY = 10f
+            // Option 3 - Tiny particles
+            val sizeX = 30f
+            val sizeY = 12f
+            // Draw the particle
+            // Option 1 - Square particles
+            //canvas.drawRect(g.position.x,g.position.y,
+                //g.position.x + sizeX,
+                //g.position.y + sizeY,
+                //paint)
+            // Option 2 - Circular particles
+            canvas.drawCircle(g.position.x, g.position.y, sizeX, paint)
+        }
+        drawing=false
+        if(afterD){
+            var angle: Double = random.nextInt(360).toDouble()
+            angle *= (3.14 / 180)
+            // Option 1 - Slow particles
+            val speed = random.nextFloat() / 3
+            // Option 2 - Fast particles
+            //val speed = (random.nextInt(10)+1);
+            val direction: PointF
+            direction = PointF(Math.cos(angle).toFloat() * speed,
+                Math.sin(angle).toFloat() * speed)
+            val g=GameObject(direction)
+            g.position.x=sp.x
+            g.position.y=sp.y
+            gameObjects.add(g)
+            afterD=false
+        }
+        if (clear){
+            gameObjects.clear()
+            clear = false
+        }
+
+    }
+
+}
