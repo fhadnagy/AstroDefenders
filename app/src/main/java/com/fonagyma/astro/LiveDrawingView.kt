ba7580a9adf1ecafbfa57456bf6173b7ecfd4ee7
fhadnagy <fonagymarci@gmail.com>
2022-11-16 20:49:51 +0100

    preparing for next move

--- app/src/main/java/com/fonagyma/astro/LiveDrawingView.kt
+++ app/src/main/java/com/fonagyma/astro/LiveDrawingView.kt
@@ -1,50 +1,30 @@
 package com.fonagyma.astro
 
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.*
 import android.util.Log
 import android.view.MotionEvent
 import android.view.SurfaceView
 
+@SuppressLint("ViewConstructor")
 class LiveDrawingView(context: Context, mScreenX : Int, mScreenY: Int): SurfaceView(context) , Runnable{
-
-    // Are we debugging?
     private val debugging = true
-    // These objects are needed to do the drawing
     private lateinit var canvas: Canvas
     private val paint: Paint = Paint()
-    // How many frames per second did we get?
     private var fps: Long = 0
-    // The number of milliseconds in a second
     private val millisInSecond: Long = 1000
-    // How big will the text be?
-    // Font is 5% (1/20th) of screen width
-    // Margin is 1.5% (1/75th) of screen width
     private val fontSize: Int = mScreenX / 20
     private val fontMargin: Int = mScreenX / 75
-    // The particle systems will be declared here later
-
-    // Here is the Thread and two control variables
     private lateinit var thread: Thread
-    // This volatile variable can be accessed
-    // from inside and outside the thread
     @Volatile
     private var drawing: Boolean = false
     private var paused = true
 
-    // These will be used to make simple buttons
-    private var resetButton: RectF
-    private var togglePauseButton: RectF
-
-    private var system : ObjectSystem
     var mP= PointF(mScreenX.toFloat()/2,mScreenY.toFloat()/2)
-
     init {
-        // Initialize the two buttons
-        resetButton = RectF(mScreenX.toFloat()-150f, 50f, mScreenX.toFloat()-150f+100f, 50+100f)
-        togglePauseButton = RectF(mScreenX.toFloat()-150f, 200f, mScreenX.toFloat()-150f+100f, 200+100f)
-        system = ObjectSystem(mP)
+
 
     }
 
@@ -59,15 +39,9 @@ class LiveDrawingView(context: Context, mScreenX : Int, mScreenY: Int): SurfaceV
             // Choose the font size
             paint.textSize = fontSize.toFloat()
             // Draw the particle systems
-            system.draw(canvas,paint)
+            //.draw()
 
-            // Draw the buttons
-            paint.color = Color.argb(255, 25, 255, 25)
-            canvas.drawRect(resetButton, paint)
-            canvas.drawRect(togglePauseButton, paint)
-            paint.color = Color.argb(255, 255, 10, 25)
-            canvas.drawRect(mP.x-10f,mP.y-10f,mP.x+10f,mP.y+10f,paint)
-            paint.color = Color.argb(255, 25, 255, 25)
+
             if (debugging) {
                 printDebuggingText()
             }
@@ -86,13 +60,7 @@ class LiveDrawingView(context: Context, mScreenX : Int, mScreenY: Int): SurfaceV
         paint.textSize = debugSize.toFloat()
         canvas.drawText("fps: $fps",
             10f, (debugStart + debugSize).toFloat(), paint)
-        canvas.drawText("No. of objects: ${system.gameObjects.size}",
-            10f, (fontMargin + debugStart +
-                    debugSize * 2).toFloat(), paint)
-        /*canvas.drawText("Particles: ${nextSystem *
-                particlesPerSystem}",
-            10f, (fontMargin + debugStart
-                    + debugSize * 3).toFloat(), paint)*/
+
     }
 
     override fun run() {
@@ -131,9 +99,7 @@ class LiveDrawingView(context: Context, mScreenX : Int, mScreenY: Int): SurfaceV
 
     private fun update() {
         // Update the particles
-
-            system.update(fps)
-
+        //updates
 
     }
 
@@ -164,26 +130,20 @@ class LiveDrawingView(context: Context, mScreenX : Int, mScreenY: Int): SurfaceV
         if (motionEvent.action and MotionEvent.
             ACTION_MASK ==
             MotionEvent.ACTION_MOVE) {
+            /**
             if (resetButton.contains(motionEvent.x,
                     motionEvent.y)) {
                 // Clear the screen of all particles
 
-            } else if (togglePauseButton.contains(motionEvent.x,
-                    motionEvent.y)) {
-
-            }else if (!paused){
-                system.addObject(
-                    PointF(motionEvent.x,
-                        motionEvent.y)
-                )
-            }
+            } */
         }
         // Did the user touch the screen
         if (motionEvent.action and MotionEvent.ACTION_MASK ==
             MotionEvent.ACTION_DOWN) {
             // User pressed the screen so let's
             // see if it was in the reset button
-            if (resetButton.contains(motionEvent.x,
+            /**TODO get a handler for all clickable surfaces
+             * if (resetButton.contains(motionEvent.x,
                     motionEvent.y)) {
                 // Clear the screen of all particles
                 system.clear= true
@@ -195,7 +155,7 @@ class LiveDrawingView(context: Context, mScreenX : Int, mScreenY: Int): SurfaceV
                     PointF(motionEvent.x,
                         motionEvent.y)
                 )
-            }
+            }*/
 
         }
         return true
